fb9dfc4eb084026a503b4b70a4905c67df9262af
delikeyki1016 <149133856+delikeyki1016@users.noreply.github.com>
2025-04-18 23:40:05 +0900

    not found 스타일일

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -3,7 +3,7 @@ import notFoundImg from "../../assets/not-found.jpg";
 const NotFound = () => {
     return (
         <div className="not-found">
-            <img src={notFoundImg} />
+            <img src={notFoundImg} alt="not found" />
         </div>
     );
 };
